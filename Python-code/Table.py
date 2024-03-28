b670a84d320ef6f0523d48374a766ef54f8a8545
Andrew <93886645+AndrewTanga@users.noreply.github.com>
2024-03-28 14:12:43 -0400

    Add files via upload

---
+++ Python-code/Table.py
@@ -0,0 +1,10 @@
+from prettytable import PrettyTable
+myTable = PrettyTable(["Student Name", "Class", "Section", "Percentage"])
+myTable.add_row(["Andrew", "X", "B", "91.2%"])
+myTable.add_row(["Jay", "X", "C", "63.5%"])
+myTable.add_row(["Harry", "X", "A", "90.23%"])
+myTable.add_row(["Bernadette", "X", "D", "92.7%"])
+myTable.add_row(["Shai", "X", "A", "98.2%"])
+myTable.add_row(["Raj", "X", "B", "88.1%"])
+myTable.add_row(["Amy", "X", "B", "95.0%"])
+print(myTable)
